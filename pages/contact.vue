95eb9f05d0b553c6a142b6b1e229f8312b2fe1ba
Dylan Dirlam <dadirlam@hotmail.com>
2020-10-28 00:32:27 -0400

    Maybe try again

--- pages/contact.vue
+++ pages/contact.vue
@@ -49,6 +49,7 @@
           Let's work together!
         </p>
         <form class="flex flex-col" name="contact" method="POST" data-netlify="true" netlify-honeypot="bot-field">
+          <input type="hidden" name="form-name" value="contact" />
           <input
             class="bg-opacity-75 rounded border text-gray-900 border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2 mb-4"
             placeholder="Name"
